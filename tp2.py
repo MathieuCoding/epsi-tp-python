bbb7ac9fbeab5f2063bf7c1872df00bee60f1122
MathieuCoding <polletmathieu.pro@gmail.com>
2023-12-02 18:52:15 +0100

    WIP

--- tp2.py
+++ tp2.py
@@ -18,41 +18,41 @@ from pprint import pprint
 # print(magreb)
 
 # Exo 3
-etudiants = {
-"etudiant_1":13,
-"etudiant_2":17,
-"etudiant_3":9,
-"etudiant_4":15,
-"etudiant_5":8,
-"etudiant_6":14,
-"etudiant_7":14,
-"etudiant_8":12,
-"etudiant_9":13,
-"etudiant_10":15,
-"etudiant_11":14,
-"etudiant_112":9,
-"etudiant_13":12,
-"etudiant_14":12,
-"etudiant_15":13,
-"etudiant_16":7,
-"etudiant_17":12,
-"etudiant_18":15,
-"etudiant_19":9,
-"etudiant_20":17
-}
-
-etudiants['etudiant_21'] = 18
-admis = {}
-non_admis = {}
-
-for i in etudiants:
-    if etudiants[i] >= 10:
-        admis[i] = etudiants[i]
-    else:
-        non_admis[i] = etudiants[i]
-
-etudiants = {'admis': admis, 'non_admis': non_admis}
-pprint(etudiants)
+# etudiants = {
+# "etudiant_1":13,
+# "etudiant_2":17,
+# "etudiant_3":9,
+# "etudiant_4":15,
+# "etudiant_5":8,
+# "etudiant_6":14,
+# "etudiant_7":14,
+# "etudiant_8":12,
+# "etudiant_9":13,
+# "etudiant_10":15,
+# "etudiant_11":14,
+# "etudiant_112":9,
+# "etudiant_13":12,
+# "etudiant_14":12,
+# "etudiant_15":13,
+# "etudiant_16":7,
+# "etudiant_17":12,
+# "etudiant_18":15,
+# "etudiant_19":9,
+# "etudiant_20":17
+# }
+#
+# etudiants['etudiant_21'] = 18
+# admis = {}
+# non_admis = {}
+#
+# for i in etudiants:
+#     if etudiants[i] >= 10:
+#         admis[i] = etudiants[i]
+#     else:
+#         non_admis[i] = etudiants[i]
+#
+# etudiants = {'admis': admis, 'non_admis': non_admis}
+# pprint(etudiants)
 
 # Exo 4
 # d = {
@@ -62,18 +62,18 @@ pprint(etudiants)
 # }
 #
 # for i in d:
-#     d[i] = sum(d[i])/len(d[i])
+#     d[i] = round(sum(d[i])/len(d[i]), 2)
 #
 # print(d)
 
 # Exo 5
-# def factoriel(number):
-#     if number == 0:
-#         return 1
-#     else:
-#         return number * factoriel(number-1)
-#
-# print(factoriel(int(input("Enter a number: "))))
+def factoriel(number):
+    if number == 0:
+        return 1
+    else:
+        return number * factoriel(number-1)
+
+print(factoriel(int(input("Enter a number: "))))
 
 # Exo 6
 # number_marks = int(input("Enter the number of marks: "))
